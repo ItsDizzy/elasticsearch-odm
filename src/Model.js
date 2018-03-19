051415ad97471da9b6538b26a45054211b64463d
ItsDizzy <itsdizzy40@gmail.com>
2018-03-19 01:00:34 +0100

    Added model base

--- src/Model.js
+++ src/Model.js
@@ -1,3 +1,26 @@
 export default class Model {
+  /**
+   * Creates a new model instance
+   * @param {String} name - The model name
+   * @param {Schema} schema - The model schema
+   */
+  constructor(name, schema) {
+    /**
+     * The model type
+     * @type {String}
+     */
+    this.type = null;
 
+    /**
+     * The model name
+     * @type {String}
+     */
+    this.name = name;
+
+    /**
+     * The model schema
+     * @type {Schema}
+     */
+    this.schema = schema;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-export { default as Client } from './Client';
-export { default as Model } from './Model';
-export { default as Schema } from './Schema';
+import Model from './Model';
+
+class ElasticSearchODM {
+  constructor() {
+    this.models = {};
+    this.options = {};
+  }
+
+  /**
+   * Gets a option value
+   * @param {String} key - Option key
+   */
+  get(key) {
+    return this.options[key];
+  }
+
+  /**
+   * Sets a option value
+   * @param {String} key - Option key
+   * @param {String|Boolean} - value Option value
+   * @returns {ElasticSearchODM}
+   */
+  set(key, value) {
+    this.options[key] = value;
+    return this;
+  }
+
+  /**
+   * Defines a model
+   * @param {String|Function} name - The model name or class extending Model
+   * @param {Schema} schema - The model schema
+   * @returns {Model}
+   */
+  model(name, schema) {
+    const model = new Model(name, schema);
+    this.models[name] = model;
+
+    return model;
+  }
+}
+
+// eslint-disable-next-line
+const odm = module.exports = exports = new ElasticSearchODM();
